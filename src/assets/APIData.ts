889589f08cf82f100cf7a3545d578392c0b146e9
alwynbarnard <barnard.alwyn262@gmail.com>
2025-08-29 14:07:58 +0200

    Spelling mistake

--- src/assets/APIData.ts
+++ src/assets/APIData.ts
@@ -253,7 +253,7 @@ export const SkillsList: string[] = [
 
 export const WorkHistoryData: WorkHistoryCompanies[] = [
   {
-    position: 'Power BI Developer',
+    position: 'BI Developer (Contract)',
     companyName: 'Medihelp',
     location: {
       city: 'Pretoria',
@@ -265,7 +265,7 @@ export const WorkHistoryData: WorkHistoryCompanies[] = [
       'At Medihelp, I played a key role in empowering analysts by helping them sharpen their SQL and Power BI skills, enabling them to get more value out of their data. I also took the initiative to design and build an automated reporting solution using Power BI, SQL, and Report Builder, which gave Medihelp critical insights into the growth and retention of financial advisers and employer groups. This work was recognized with a Spot Award, highlighting the impact of the report on the business. Additionally, I developed a Power Apps solution to streamline the process of capturing and managing employee skills and competencies, which improved data accuracy and accessibility across the organization. My contributions helped enhance data-driven decision-making and operational efficiency at Medihelp.',
   },
   {
-    position: 'Software Engineer',
+    position: 'Intermediate Front End Developer (Contract)',
     companyName: 'SprintHive',
     location: {
       city: 'Johannesburg',
@@ -277,8 +277,8 @@ export const WorkHistoryData: WorkHistoryCompanies[] = [
       'At SprintHive, I collaborated with senior product owners to analyze and optimize the currentecosystem, focusing on delivering data-driven improvements that enhanced usability andperformance. I led a project to streamline customer onboarding and identity verification processeson mobile devices, particularly in low-income, network-restricted areas, using analytical insights toimprove efficiency and accessibility.My contributions included cost analysis and identifying key areas to reduce expenses, which directlyalleviated user strain and improved profitability. Additionally, I provided business intelligence supportby training and upskilling business analysts on interpreting data and leveraging insights related toproduct offerings and enhancements.',
   },
   {
-    position: 'BI & Data Analyst Developer',
-    companyName: 'Various - Freelance',
+    position: 'Intermediate Front End Analyst Developer',
+    companyName: 'Freelance',
     location: {
       city: 'Pretoria',
       country: 'South Africa',
@@ -301,7 +301,7 @@ export const WorkHistoryData: WorkHistoryCompanies[] = [
       'Developed new features for Betway’s mobile and Desktop sites using VueJS with Vuetify and Nuxt and provided support and improvements to the current site using MVC, WebAPI and Bootstrap. Streamlined the process of tracking tasks using Azure DevOps and automated the process of alerting potential burnout. Spearheaded team interaction through daily check-ins and task updates. Managed events such as team building and testing events. Spearheaded code documentation and collaboration project which includes designs, wireframes, development documentation and feedback from a central location. Provided technical support and training for colleagues from various teams as well as new hires. Developed quality assurance protocols for various projects. Optimized existing systems to better meet client needs. Streamlined the maintenance and updating of existing and new systems.',
   },
   {
-    position: 'Junior Software Engineer - Full Stack',
+    position: 'Junior Software Engineer',
     companyName: 'Ke Concepts',
     location: {
       city: 'Johannesburg',
